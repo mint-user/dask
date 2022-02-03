5fbb1048e1cd613b0c71c81f17fd0e61eed2a9e6
xander_pronin <xanderpronin@gmail.com>
2022-02-03 23:01:38 +0300

    making login + tests

--- app/mod_auth/controllers.py
+++ app/mod_auth/controllers.py
@@ -4,7 +4,7 @@ from app import app, db
 from app.mod_auth.models import User
 from flask import request
 from flask_bcrypt import Bcrypt
-
+from datetime import timedelta, datetime
 
 bcrypt = Bcrypt(app)
 
@@ -23,19 +23,36 @@ def update_user():
 
 @app.route('/api/v1/login', methods=['POST'])
 def login():
-    # email exists
     print(request.data)
+    # check request structure
+    if 'email' not in request.json or 'password' not in request.json:
+        return {"error": "Bad request"}, 400
+
+    # email exists
     email = request.json['email']
     user = User.query.filter(User.email == email).first()
     if user is None:
-        return {"error": "Wrong email or password"}, 202
-    else:
-        pass_hash = user.password
-        if bcrypt.check_password_hash(pass_hash, request.json['password']):
-            token = secrets.token_hex(16)
-            user.token = token
-            #
-            return str(request.data), 201
+        return {"error": "Wrong email or password"}, 401
+
+    # check password
+    pass_hash = user.password
+    if not bcrypt.check_password_hash(pass_hash, request.json['password']):
+        return {"error": "Wrong email or password"}, 401
+
+    # generate token
+    token = secrets.token_hex(16)
+
+    now = datetime.now()
+    delta = timedelta(hours=1)
+    token_expires = (now + delta)
+
+    user.token = token
+    user.token_expires = token_expires
+
+    db.session.add(user)
+    db.session.commit()
+    return {"token": token, "token_expires": token_expires.strftime('%Y-%m-%d %H:%M:%S')}, 201
+
 
 
 @app.route('/api/v1/accounts', methods=['POST'])
